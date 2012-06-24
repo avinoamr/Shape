d03dcab328a31e4bec22a02a97c73e979d601e61
Roi Avinoam <avinoamr@gmail.com>
2012-06-24 19:58:07 +0300

    Animation is (buggy, disgusting, hacky, patchy, wrong, and ... ) working\!

--- src/Animation.js
+++ src/Animation.js
@@ -1,28 +1,39 @@
-define([ "../libs/underscore", "../src/TimelineAnimation", "../src/Tween" ], function() {
+define([ "../libs/underscore", 
+         "../src/TimelineAnimation", 
+         "../src/Tween",
+         "../src/Parameters",
+         "../src/Events"
+
+         ], function() {
 
 
     //
     _.extend(Shape.prototype, {
 
         //
-        animations: function() {
+        animation: function( animation ) {
 
-            return this._setget( '_animations', arguments );
+            if ( 0 == arguments.length ) {
+                return this._default( '_animation', {} )._animation;
+            }
 
-        },
+            if ( "number" == typeof +_.keys( animation )[ 0 ] ) {
+                var animation = { "default": animation };
+            }
 
-        //
-        animations_add: function( name, animation ) {
+            for ( var name in animation ) {
+                ( animation[ name ] instanceof Shape.Animation ) || ( animation[ name ] = new Shape.Animation( animation[ name ] ) );
+            }
 
-        },
-
-        //
-        animations_remove: function( name ) {
+            this._animation = animation;
 
         },
 
         // 
         play: function( name ) {
+
+            name || ( name = "default" );
+            this._animation[ name ].play( this );
 
         },
 
@@ -47,6 +58,102 @@ define([ "../libs/underscore", "../src/TimelineAnimation", "../src/Tween" ], fun
         func.animatable = true;
         return func;
 
-    }
+    };
+
+
+
+    var apply_state = function ( shape, state ) {
+
+        for ( var name in state ) {
+            debugger;
+            shape[ name ].call( shape, state[ name ] );
+        }
+
+        return shape;
+
+    };
+
+    //
+    Shape.Animation = Class.extend( Shape.Parameters, Shape.Events, {
+
+        //
+        constructor: function( timeline ) {
+
+            this.timeline( timeline );
+
+        },
+
+        //
+        timeline: function( timeline ) {
+
+            if ( 0 == arguments.length ) {
+                return this._timeline;
+            }
+
+            this._timeline = {};
+            for ( var frame in timeline ) {
+                this.frame( frame, timeline[ frame ] );
+            }
+
+            return this;
+
+        },
+
+        //
+        frame: function( frame, state ) {
+
+            if ( 1 == arguments.length ) {
+                return this._timeline[ frame ];
+            }
+
+            this._default( 'timeline.' + frame, {} );
+            this._timeline[ frame ] = state;
+            return this;
+
+        },
+
+        loop: function( shape ) {
+
+            this.play( shape ).on( "complete", function() {
+                this.loop( shape );
+            }, this );
+
+        },
+
+        // 
+        play: function( shape, frame_id ) {
+            
+            frame_id || ( frame_id = 0 );
+            var that = this;
+            var timeline = this.timeline();
+            var frames = _.map( _.keys( timeline ), function( frame ) { return +frame } );
+            frames.sort();
+
+            var frame = frames[ frame_id || 0 ];
+            var state = timeline[ frame ];
+
+            if ( 0 == frame ) {
+                apply_state( shape, state );
+                return this.play( shape, ++ frame_id );
+            }
+
+            var animate = shape.tween( frame );
+            apply_state( animate, state );
+            animate.on( "complete", function() {
+
+                var next_frame = frames[ ++frame_id ];
+                if ( next_frame ) {
+                    that.play.apply( that, shape, frame_id );
+                } else {
+                    that.trigger( "complete" );
+                }
+
+            }).start();
+
+            return this;
+
+        }
+
+    });
 
 });
