c0408a152850c855536a3904d2a73b1c7f8b3bd5
Roi Avinoam <avinoamr@gmail.com>
2013-08-09 19:32:52 +0300

    Mouse events - work in progress

---
+++ examples/10_interactive/Main.js
@@ -0,0 +1,155 @@
+
+require.config({
+    paths: {
+        "Shape": "../../src"
+    },
+
+    // cache bust for require js
+    urlArgs: ( "#nobust" == location.hash) ? "" : "bust=" +  (new Date()).getTime()
+
+});
+
+define([
+    
+    "Shape/Shape",
+    "../Stats"
+
+], function( Shape, Stats ) {
+
+    var canvas = document.createElement( 'canvas' );
+    document.body.appendChild( canvas );
+
+    //
+    var label = new Shape({
+        sid: "label",
+        position: { x: Shape.CENTER, y: Shape.CENTER },
+        text: {
+            content: "Click a Button",
+            background: "white",
+            font: "20px Arial Black"
+        }
+    });
+
+    //
+    var btn1 = new Shape({
+        sid: "btn1",
+        background: "blue", 
+        position: { x: Shape.LEFT },
+        size: { x: 150, y: 40 },
+        children: [
+            {
+                position: { x: Shape.CENTER, y: 5 },
+                text: {
+                    content: "Button 1",
+                    background: "white",
+                    font: "20px Arial Black"
+                }
+            }
+        ]
+    }).on( "mouse:move", function() {
+        console.log( this.sid, "MOVING" );
+    });
+
+    //
+    var btn2 = new Shape({
+        sid: "btn2",
+        background: "blue", 
+        position: { x: Shape.CENTER },
+        size: { x: 150, y: 40 },
+        children: [
+            {
+                position: { x: Shape.CENTER, y: 5 },
+                text: {
+                    content: "Button 2",
+                    background: "white",
+                    font: "20px Arial Black"
+                }
+            }
+        ]
+    }).on( "mouse:move", function() {
+        console.log( this.sid, "MOVING" );
+    });
+
+    //
+    var btn3 = new Shape({
+        sid: "btn3",
+        background: "blue", 
+        position: { x: Shape.RIGHT },
+        size: { x: 150, y: 40 },
+        children: [
+            {
+                position: { x: Shape.CENTER, y: 5 },
+                text: {
+                    content: "Button 3",
+                    background: "white",
+                    font: "20px Arial Black"
+                }
+            }
+        ]
+    }).on( "mouse:move", function() {
+        console.log( this.sid, "MOVING" );
+    });
+
+    //
+    var app = new Shape({
+        sid: "app",
+        size: { x: 500, y: 500 },
+        children: [ btn1, btn2, btn3, label ]
+    }).on( "mouse:move", function() {
+        console.log( this.sid, "MOVING" );
+    });
+
+    app.canvas( canvas );
+
+    var shape_capture = function( point ) {
+
+        var position = this.position(),
+            size = this.size();
+
+        // translate the point to local shape coordinates
+        point = {
+            x: point.x - position.x,
+            y: point.y - position.y
+        };
+
+        if ( point.x < 0 || point.y < 0 || point.x > size.x || point.y > size.y ) {
+            return; // point isn't contained by this shape
+        }
+        
+        // check the children
+        var children = this.children();
+        for ( var i = 0 ; i < children.length ; i += 1 ) {
+            shape_capture.call( children[ i ], point );
+        }
+
+        if ( this.is_listening( "mouse:move" ) ) {
+            this.trigger( "mouse:move" );
+        }
+    };
+
+    canvas.onmousemove = function( ev ) {
+        var box = { top: 0, left: 0 };  
+        if ( this.getBoundingClientRect ) {
+            box = this.getBoundingClientRect();
+        }
+
+        var docElem = document.documentElement;
+        box = {
+            top: box.top + window.pageYOffset - docElem.clientTop,
+            left: box.left + window.pageXOffset - docElem.clientLeft
+        };
+
+        var mouse = {
+            x: ev.pageX - box.left,
+            y: ev.pageY - box.top
+        }
+
+        shape_capture.call( app, mouse );
+    };
+
+    Stats.monitor( app ).renderloop();
+
+    window.app = app;
+    window.Shape = Shape;
+
+});
