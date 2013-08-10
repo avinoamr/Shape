26791295f50f345a32f9f6439a5f601826c15430
Roi Avinoam <avinoamr@gmail.com>
2013-08-11 02:39:24 +0300

    Hiding shape wasn't possible because visibility defaulted to true. Fixed here

--- src/Visibility.js
+++ src/Visibility.js
@@ -7,33 +7,26 @@ define([], function() {
     return {
 
         _mixin_shape: true,
+        _visibility: true,
 
         //
         visibility: function( visibility ) {
-
-            return this._setget( "_visibility", arguments, "visibility" ) || true;
-
+            return this._setget( "_visibility", arguments, visibility, "visibility" );
         },
 
         //
         show: function() {
-
             return this.visibility( true );
-
         },
 
         //
         hide: function() {
-
             return this.visibility( false );
-
         },
 
         //
         toggle: function() {
-
             return this.visibility( !this.visibility() );
-
         }
 
     };
